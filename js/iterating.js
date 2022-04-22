c3b3b73d7f06f3eda7286583940b2c6244688f7b
Alex Rodriguez <alexander.joshua.rodriguez@gmail.com>
2022-04-21 20:25:04 -0500

    Update to iterating_arrays exercises

---
+++ js/iterating.js
@@ -0,0 +1,72 @@
+(function(){
+    "use strict";
+
+    /**
+     * TODO:
+     * Create an array of 4 people's names and store it in a variable called
+     * 'names'.
+     */
+    let fourNames = ['Alex', 'Michelle', 'Bill', 'Gabriela'];
+
+    /**
+     * TODO:
+     * Create a log statement that will log the number of elements in the names
+     * array.
+     */
+    console.log(fourNames.length);
+    /**
+     * TODO:
+     * Create log statements that will print each of the names individually by
+     * accessing each element's index.
+     */
+    console.log(fourNames[0])
+    console.log(fourNames[1])
+    console.log(fourNames[2])
+    console.log(fourNames[3])
+
+    /*
+     * TODO:
+     * Write some code that uses a for loop to log every item in the names
+     * array.
+     */
+    for(let i = 0; i < fourNames.length; i++){
+        console.log(fourNames[i])
+    }
+    /**
+     * TODO:
+     * Refactor your above code to use a `forEach` loop
+     */
+    fourNames.forEach(function (name){
+        console.log(name);
+    })
+    /**
+     * TODO:
+     * Create the following three functions, each will accept an array and
+     * return an an element from it
+     * - first: returns the first item in the array
+     * - second: returns the second item in the array
+     * - last: returns the last item in the array
+     *
+     * Example:
+     *  > first([1, 2, 3, 4, 5]) // returns 1
+     *  > second([1, 2, 3, 4, 5]) // returns 2
+     *  > last([1, 2, 3, 4, 5]) // return 5
+     */
+
+    function first(param){
+        return param[0];
+    }
+
+    function second(param){
+        return param[1];
+    }
+
+    function third(param){
+        return param[param.length - 1];
+    }
+
+    console.log(first(fourNames));
+    console.log(second(fourNames));
+    console.log(third(fourNames));
+
+})();
