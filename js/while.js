0e0f49e4a5a7e9a44e1c68e2c4709208dddae03a
Alex Rodriguez <alexander.joshua.rodriguez@gmail.com>
2022-04-21 19:44:42 -0500

    Update to while_js exercises

--- js/while.js
+++ js/while.js
@@ -33,21 +33,33 @@
     An ice cream seller can't go home until she sells all of her cones. First write enough code that generates a random number between 50 and 100 representing the amount of cones to sell before you start your loop. Inside of the loop your code should generate another random number between 1 and 5, simulating the amount of cones being bought by her clients. Use a do-while loop to log to the console the amount of cones sold to each person. The below code shows how to get the random numbers for this exercise.
 
      */
-
+    // Function that generates a random number to determine how many cones the seller needs to sell
     let amountToSell = function(){
         return Math.floor((Math.random() * (100 - 50) + 50));
     }
-
+    // Function that generates a random number to determine how many cones are being bought by customers
     let conesBought = function(){
         return Math.floor((Math.random() * (6 - 1) + 1));
     }
-
+    // Main function
     function iceCreamSeller(){
         let amount = amountToSell()
+        let customerBought;
+        console.log(amount + " cones to end the day.")
         do {
-            let customer = conesBought();
-        }while()
+            customerBought = conesBought();
+            if(amount < customerBought){
+                console.log("Sorry can't sell you " + customerBought + " cones. Only have " + amount + " left")
+            }
+            else if(amount === 0){
+                console.log("Yay, I sold all cones");
+            } else {
+                amount -= customerBought
+                console.log(customerBought + " cones sold. " + amount + " cones left.")
+            }
+        } while(amount > 0);
     }
 
+    iceCreamSeller();
 
 })()
