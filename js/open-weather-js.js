a76dc0966dfd44209439f05918aee8cdfdf49791
Alex Rodriguez <alexander.joshua.rodriguez@gmail.com>
2022-05-28 14:03:01 -0500

    update to openWeather exercise

--- js/open-weather-js.js
+++ js/open-weather-js.js
@@ -1,5 +1,9 @@
 "use strict";
 
+function capitalize(s) {
+    return s[0].toUpperCase() + s.slice(1);
+}
+
 const URL = 'https://api.openweathermap.org/data/2.5/onecall'
 
 
@@ -11,6 +15,9 @@ geocode("San Antonio, US", MAPBOX_API_KEY).then(function (results) {
         lon: results[0],
         units: 'imperial'
     }).done(function (results){
+        $('#temp').html("Current Temperature: " + results.current.temp + "&#8457;")
+        $('#description').html(capitalize(results.current.weather[0].description))
+        $('#icon').css("background-image", "url('http://openweathermap.org/img/w/" + results.daily[0].weather[0].icon + ".png')")
         console.log(results)
     })
 })
