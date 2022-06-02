1ee326847ac6971ce74a1922d7e1df41ea9cbf9f
Alex Rodriguez <alexander.joshua.rodriguez@gmail.com>
2022-06-02 15:13:50 -0500

    update

--- js/es6.js
+++ js/es6.js
@@ -78,6 +78,8 @@ let list = '<ul>';
 // TODO: rewrite the following loop to use a for..of loop
 for(let dev of developers) {
   // TODO: rewrite the assignment below to use template strings
-  list += `<li> developer </li>`;
+  list += `<li> ${dev} </li>`;
 }
 list += '</ul>';
+
+document.querySelector('body').innerHTML += list
