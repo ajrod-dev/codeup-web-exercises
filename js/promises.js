59454c0800f1cbaf49f3744afac78f1ddc886d41
Alex Rodriguez <alexander.joshua.rodriguez@gmail.com>
2022-06-07 09:18:12 -0500

    warmup exercise

--- js/promises.js
+++ js/promises.js
@@ -1,16 +1,15 @@
 "use strict";
 
-const myToken = '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';
-
-
-
 
 function myPromise(username){
     const url = `https://api.github.com/users/${username}/events/public`;
-    return fetch(url, {headers: {'Authorization': `token ${myToken}`}}).then((resp) => {
+    return fetch(url, {headers: {'Authorization': `token ${myGithubToken}`}}).then((resp) => {
         return resp.json()
     }).then((data) => {
+        console.log(data)
         return new Date(data[0].created_at).toDateString()
+    }).catch((err) => {
+        console.log(err)
     })
 }
 
